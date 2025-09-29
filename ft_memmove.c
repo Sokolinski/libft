f8a46be8710bc32b2f223d9f74ba86df25ca9a68
Mikhail Sakalinski <msakalin@c3r6s2.42warsaw.pl>
2025-09-29 17:50:06 +0200

    all

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,39 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memmove.c                                          :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msakalin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: msakalin <msakalin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:06:28 by msakalin          #+#    #+#             */
-/*   Updated: 2025/09/29 14:07:10 by msakalin         ###   ########.fr       */
+/*   Updated: 2025/09/29 17:49:46 by msakalin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stddef.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t i;
-    char *to;
-    char *from;
-    char tmp[n];
-
-    to = (char *) dest;
-    from = (char *) src;
-    i = 0;
-
-    while (i < n)
-    {
-        tmp[i] = from[i];
-        i++;
-    }
-    i = 0;
-    while (i < n)
-    {
-        to[i] = tmp[i];
-        i++;
-    }
-
-    return (dest);
+	size_t	i;
+	char	*to;
+	char	*from;
+	char	tmp[n];
+	
+	to = (char *) dest;
+	from = (char *) src;
+	i = 0;
+	while (i < n)
+	{
+		tmp[i] = from[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		to[i] = tmp[i];
+		i++;
+	}
+	return (dest);
 }
-
