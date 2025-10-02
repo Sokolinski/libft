69eb9f88af21beeb0357dfafe04fdcece622aed4
Mikhail Sokalinski <sokolinsky.com13@gmail.com>
2025-10-02 14:06:55 +0200

    added ft_tolower

--- libft.h
+++ libft.h
@@ -3,9 +3,10 @@
 
 #include <stddef.h>
 
-int ft_strlen(char *c);
+int	ft_strlen(char *c);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	strlcat(char *dst, const char *src, size_t size);
-int ft_toupper(int ch);
+int	ft_toupper(int ch);
+int	ft_toupper(int ch);
 
 #endif LIBTF_H
