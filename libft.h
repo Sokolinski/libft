1133b0156e0c69b7826583230dc08623976079c1
Mikhail Sokalinski <sokolinsky.com13@gmail.com>
2025-10-07 09:08:57 +0200

    change

--- libft.h
+++ libft.h
@@ -14,5 +14,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 size_t	strlen(const char *s);
 int ft_memcmp( const void* p1, const void* p2, size_t n);
-char	*strstr (const char *str_1, const char *str_2);
+char	*ft_strstr(const char *str_1, const char *str_2);
 #endif
